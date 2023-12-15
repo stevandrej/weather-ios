4df0c9ef13591fb9522e97ed7d7c1d43ade17f32
Andrej Stevkovski <stevandrej@gmail.com>
2023-12-15 23:42:06 +0100

    button expand

--- src/components/WeatherForecast/WeatherForecast.tsx
+++ src/components/WeatherForecast/WeatherForecast.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import CardForecast from "../CardForecast/CardForecast";
 import styles from "./weatherForecast.module.css";
 import { HourlyWeatherResponse } from "@/types/forecast";
@@ -7,11 +8,21 @@ export default function WeatherForecast({
 }: {
 	hourlyWeather: HourlyWeatherResponse;
 }) {
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
-		<div className={styles.container}>
-			{hourlyWeather.list.map((day) => (
-				<CardForecast weather={day} key={day.dt} />
-			))}
+		<div className={`${styles.container} ${isOpen && styles.expanded}`}>
+			<button
+				className={styles.expandBtn}
+				onClick={() => setIsOpen(!isOpen)}
+			>
+				<div className={styles.expandIcon} />
+			</button>
+			<div className={styles.hourly}>
+				{hourlyWeather.list.map((day) => (
+					<CardForecast weather={day} key={day.dt} />
+				))}
+			</div>
 		</div>
 	);
 }
